680da61779b2c9238430cf88c740301d295329ad
Vincent Palomares <vincent.palomares@intel.com>
2014-03-26 09:50:37 -0500

    iMinor changes (debug).
    Former-commit-id: b9a8525622777805c4ae4f7349808dd2b6588f93 [formerly b7b5e5ca3751e517b1174d908726f5bf4f891526] [formerly ad5a26dd46f2baec0546a8a64f2dcfb9b66d3d37 [formerly 24b3cb4014e6241b596a79d0a4832fff7825b237]]
    Former-commit-id: bf0ad26ebd17a9c95cd6955d60a52cbd748016aa [formerly d0d9004c61584c9816363b94e9a6b8df6c0d1f12]
    Former-commit-id: 3de7e677e228a88d9357f9160ab6c0ba317d19ea

--- generate_variants.sh
+++ generate_variants.sh
@@ -44,7 +44,8 @@ do
 				echo "Configuration error. Aborting."
 				exit -1
 			fi
-			$DECAN "$DECAN_CONFIGURATION" &> /dev/null
+			$DECAN "$DECAN_CONFIGURATION" &> decan_output
+#/dev/null
 
 			generated_binaries=$( grep generated $PWD/$DECAN_REPORT | cut -f2 -d' ' )
 			if [[ "$generated_binaries" == "" ]]
