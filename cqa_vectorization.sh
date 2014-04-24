98fa4113e8374c3839ce9b2a6af3465572287f6b
Vincent Palomares <vincent.palomares@intel.com>
2014-04-24 15:09:50 -0500

    Improved cqa_vectorization.sh.
    Former-commit-id: db1226c17d72ddd22170f1a93eae094f2a01cea6 [formerly d2c0793e375977c37a28beab4e6dfe6510fbc57d] [formerly 117030586a100d473241634eddac03495a4d7595 [formerly 60ef089cc0354998320b97c1fbd8a4ce6ee06eab]]
    Former-commit-id: 6735f38233cc92bea6615b85ec02127dbc2442b7 [formerly cedf38135391211c382cbe18df68ae3ea8e63b12]
    Former-commit-id: ac70bccff3f85ea87641a0717fbf7d4fbdfc654d

--- cqa_vectorization.sh
+++ cqa_vectorization.sh
@@ -5,10 +5,13 @@ source ./const.sh
 bin_path="$1"
 loop_id="$2"
 
-rm -f *.csv
-for mode in MEM_VEC COMPUTE_VEC FULLY_VEC VECTOR_ISET FP FP_ARITH
+mega_header=""
+mega_content=""
+#for mode in MEM_VEC COMPUTE_VEC FULLY_VEC VECTOR_ISET FP FP_ARITH
+for mode in MEM_VEC FULLY_VEC VECTOR_ISET FP FP_ARITH
 do
-	for option in none force_sse
+#	for option in none force_sse
+	for option in force_sse
 	do
 		if [[ "$option" != "none" ]]
 		then
@@ -20,8 +23,26 @@ do
 		echo "Mode: $mode, option arg: $option_arg"
 		echo "\"$MAQAO\" module=cqa bin=\"$bin_path\" loop=\"$loop_id\" uarch=SANDY_BRIDGE of=csv -ext im=$mode $option_arg"
 		"$MAQAO" module=cqa bin="$bin_path" loop="$loop_id" uarch=SANDY_BRIDGE of=csv -ext im=$mode $option_arg
-		mv loops.csv "if_vec_${mode}_$option.csv"
+		header=$( head -n 1 loops.csv | sed 's/;$//g' | sed "s/;/;V($mode)($option)_/g" | sed "s/^/V($mode)($option)_/g" )
+		content=$( tail -n 1 loops.csv | sed 's/;$//g' )
+
+		rm -f loops.csv
+
+		if [[ "$header" != "" && "$content" != "" ]]
+		then
+			if [[ "$mega_header" == "" ]]
+			then
+				mega_header="$header"
+				mega_content="$content"
+			else
+				mega_header="$mega_header;$header"
+				mega_content="$mega_content;$content"
+			fi
+		fi
 
 		echo
 	done
 done
+
+echo "$mega_header"
+echo "$mega_content"
