b25251e241bb9155471f3c2f6748ce4e942d0142
Vincent Palomares <vincent.palomares@intel.com>
2014-06-27 16:55:49 -0500

    Fixed typo in OoO call of ooo_analysis.sh.
    Former-commit-id: ad51c3f632c2c9f22eb8209291f08819c826cfa7 [formerly 25dfe332921be683c499dd5f0d352144156d456c] [formerly 0fb704e87ffb554e617761ddf16b33d867bb0bb2 [formerly ad71a4844d15d55e3bbe861dc318e7fb37231667]]
    Former-commit-id: 297aba1ff37b2a1c1be6cf597bc7b068b9c74bed [formerly b1e4fcdc51c3f9c6aa5f05f4d8b74471cedde047]
    Former-commit-id: fc47f89f6bc33d19341506e6f2b4d874cf3c18d7

--- ooo_analysis.sh
+++ ooo_analysis.sh
@@ -25,28 +25,28 @@ echo "OoO_${uarch_suffix}_normal;OoO_${uarch_suffix}_normal_stalls;OoO_${uarch_s
 
 name=$( basename "$loop_file" | sed 's/_cpi.snb.ooo//g' | sed 's/_cpi.hsw.ooo//g' )
 
-output=$( "$bin" "$loop_file" --verbosity 0 uarch="$local_uarch" $ooo_args )
+output=$( "$bin" "$loop_file" --verbosity 0 --uarch="$local_uarch" $ooo_args )
 res=$( echo "$output" | grep iteration: | cut -f2 -d':' | sed 's/ //g' )
 main_culprits=$( echo "$output" | grep "Stalls per iteration" | sed 's/Stalls per iteration \[\([^\]*\)\]/\1/g' )
 
 
-output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args uarch="$local_uarch" --rs_size 1000 --rob_size 1000 --prf_int_size 1000 --prf_float_size 1000 --prf_max 1000 --branch_buffer_size 1000 --load_buffer_size 1000 --store_buffer_size 1000 )
+output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args --uarch="$local_uarch" --rs_size 1000 --rob_size 1000 --prf_int_size 1000 --prf_float_size 1000 --prf_max 1000 --branch_buffer_size 1000 --load_buffer_size 1000 --store_buffer_size 1000 )
 res_infinite=$( echo "$output" | grep iteration: | cut -f2 -d':' | sed 's/ //g' )
 gain_infinite=$( echo "$res - $res_infinite" | bc )
 infinite_culprits=$( echo "$output" | grep "Stalls per iteration" | sed 's/Stalls per iteration \[\([^\]*\)\]/\1/g' )
 
 if [[ "$local_uarch" == "sandy_bridge" ]]
 then
-	output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args uarch="$local_uarch" --rs_size 27 --rob_size 84 --prf_int_size 65 --prf_float_size 55 --prf_max 74 --branch_buffer_size 24 --load_buffer_size 32 --store_buffer_size 18 )
+	output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args --uarch="$local_uarch" --rs_size 27 --rob_size 84 --prf_int_size 65 --prf_float_size 55 --prf_max 74 --branch_buffer_size 24 --load_buffer_size 32 --store_buffer_size 18 )
 else
-	output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args uarch="$local_uarch" --rs_size 30 --rob_size 96 --prf_int_size 69 --prf_float_size 67 --prf_max 137 --branch_buffer_size 24 --load_buffer_size 36 --store_buffer_size 21 )
+	output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args --uarch="$local_uarch" --rs_size 30 --rob_size 96 --prf_int_size 69 --prf_float_size 67 --prf_max 137 --branch_buffer_size 24 --load_buffer_size 36 --store_buffer_size 21 )
 fi
 res_halved=$( echo "$output" | grep iteration: | cut -f2 -d':' | sed 's/ //g' )
 penalty_halved=$( echo "$res_halved - $res" | bc )
 halved_culprits=$( echo "$output" | grep "Stalls per iteration" | sed 's/Stalls per iteration \[\([^\]*\)\]/\1/g' )
 
 
-output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args uarch="$local_uarch" --load_latency 40 )
+output=$( "$bin" "$loop_file" --verbosity 0 $ooo_args --uarch="$local_uarch" --load_latency 40 )
 res_long_latency=$( echo "$output" | grep iteration: | cut -f2 -d':' | sed 's/ //g' )
 penalty_long_latency=$( echo "$res_long_latency - $res" | bc )
 long_latency_culprits=$( echo "$output" | grep "Stalls per iteration" | sed 's/Stalls per iteration \[\([^\]*\)\]/\1/g' )
