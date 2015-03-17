e8a53093b69dfc86aef2eb9de55a85e57880dba9
david.c.wong <cwong29@fxilab148.fx.intel.com>
2015-03-17 14:21:20 -0500

    Cleaned up counter list building in run_codelet.sh and added some varables in const.sh to pick counters more easily.
    Former-commit-id: 964e8a374b0b0fcfefebef34e36ae92bf1732c96 [formerly c45eab9826eab22a5ef12c34d90efdee4dddea29] [formerly abace5709a1335a12fef63c8fca032295853eba6 [formerly 6a83b9a184b5dd775bac3ee4f21c3172f366a5ab]]
    Former-commit-id: 01607f79a45658b9f071e265bc7908807d739181 [formerly 0d8f0e03cba1c01f8b2964da497c757ac9fbd32c]
    Former-commit-id: bd3d2762619b5aed2938759f80ef2523a803dff1

--- vrun_fxilab148.sh
+++ vrun_fxilab148.sh
@@ -7,8 +7,11 @@ variants="REF LS FP DL1 FES"
 variants="REF LS FP"
 variants="REF"
 #linear_sizes="2000 10000000"
-linear_sizes="1000 2000"
+#linear_sizes="1000 2000"
 #linear_sizes="1000 2000 4000 6000 8000 10000 20000 40000 60000 80000 100000 200000 400000 600000 800000 1000000 2000000 4000000 6000000 8000000 10000000"
+#linear_sizes="1000000 2000000 4000000 6000000 8000000 10000000"
+linear_sizes="1000000 4000000 8000000 10000000"
+#linear_sizes="1000 2000 4000 6000 8000 10000 20000 40000"
 quadratic_sizes="208 240 304 352 400 528 608 704 800 928 1008 1100 1200 1300 1400 1500 1600 1800 2000 2500 3000"
 #memory_loads="0 99999"
 memory_loads="0"
@@ -20,9 +23,46 @@ quadratic_codelets=""
 
 
 prefix="/nfs/fx/home/cwong29/working/NR-scripts"
-#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/balanc_3/balanc_3_de"
+ubmkprefix="${prefix}/nr-codelets/bws/nr_ubmks"
+
+#linear_codelets="${ubmkprefix}/*"
+#linear_codelets="$linear_codelets ${ubmkprefix}/balanc_3_1_ubmk_de"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_ls_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_st_only_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_st_1sonly_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_ld_1sonly_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_ld_bigstride_1sonly_se"
+
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_st_bigstride_1sonly_se"
+
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_ldst_1sonly_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_ldst_no_pxor_1sonly_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/s319_se"
+#linear_codelets="$linear_codelets ${ubmkprefix}/mprove_9_ubmk_de"
+
+linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/balanc_3/balanc_3_de"
 #linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/mprove_9/mprove_9_de"
-linear_codelets="$linear_codelets ${prefix}/intel_codelets/1D_loop-Stride_1/s319/s319_se"
+#linear_codelets="$linear_codelets ${prefix}/intel_codelets/1D_loop-Stride_1/s319/s319_se"
+
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/hqr_13/hqr_13_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/tridag_2/tridag_2_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/realft_4/realft_4_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/four1_2/four1_2_me"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/toeplz_1/toeplz_1_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/toeplz_2/toeplz_2_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/toeplz_4/toeplz_4_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/toeplz_1/toeplz_1_sU1_sVS_de"
+#linear_codelets="$linear_codelets ${prefix}/nr-codelets/numerical_recipes/1D_loop-Stride_1/balanc_3/balanc_3_sU1_sVS_de"
+#linear_codelets="$linear_codelets ${ubmkprefix}/tridag_2r_de"
+#linear_codelets="$linear_codelets ${ubmkprefix}/tridag_2r_1a_de"
+#linear_codelets="$linear_codelets ${ubmkprefix}/tridag_2r_1a_1_de"
+
+
+
+
+
+
+
 
 #quadratic_codelets="$quadratic_codelets /localdisk/amazouz/ecr_codelets/nr-codelets/numerical_recipes/2D_loop-Stride_1/matadd_16/matadd_16_sU1_sVS_de"
 #quadratic_codelets="$quadratic_codelets /localdisk/amazouz/ecr_codelets/nr-codelets/numerical_recipes/2D_loop-Stride_1/matadd_16/matadd_16_sVS_de"
