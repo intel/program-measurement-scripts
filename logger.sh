407797a7d0d8a80ddf851e7c83c5a3601fdc5734
david.c.wong <cwong29@fxilab148.fx.intel.com>
2015-04-29 17:50:27 -0500

    New logger script to write log
    Former-commit-id: ac6b88aaf4a28f7415ab83b79910945fb3c693c0 [formerly 1ac24c08e96e8951b0e31083ba787074eb3d3f65] [formerly 5fd80752b39707b3b42fcf86b1d16519057afa75 [formerly f07f7a8cc071a96f84e871756093ec697c2b4d65]]
    Former-commit-id: e299641743c7daad552541a3b54338442deafdb5 [formerly 8152964bd864e652200e1ecd61415f1b8ff368f2]
    Former-commit-id: 608e0e93401bfe2ffe1eb711525286eb44efbadd

---
+++ logger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+source ./const.sh
+
+if [[ "$nb_args" != "2" ]]
+then
+	echo "ERROR! Invalid arguments (need: run id, message)."
+	exit -1
+fi
+
+runid="$1"
+msg="$2"
+
+if [ ! -d ${LOG_FOLDER} ]; then
+    mkdir ${LOG_FOLDER}
+fi
+
+echo "[${runid}] ${msg}" >> ${LOG_FILE}
