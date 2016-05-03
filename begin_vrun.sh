49d1732cffb4ca158c23aece8ac3bc9c821daea3
David Wong <david.c.wong@intel.com>
2016-05-03 17:57:43 -0500

    New files for NR runs.
    Former-commit-id: 36f11214da2be232afe7839bd5747218ea0b1f16 [formerly f469a87c4c32842b48d96db6a29f4891448cdc36] [formerly a00779e798a838af6d036ae28af86e770289e24f [formerly 61e1d76b162dd0b9091f967c410f0c09a7e1e4e8]]
    Former-commit-id: c2656e1e84acba89a0fb16e9c73b1c3d64475c92 [formerly dea6fa82437fa2233be1137411735b6b8cd04998]
    Former-commit-id: 6e26040ca00e4e19adf088651207d2b9c1d55cb3

---
+++ begin_vrun.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+source $(dirname $0)/const.sh
+
+
+if [[ "$nb_args" > "1" ]]
+then
+	echo "ERROR! Invalid arguments (need: run description (optional))."
+	exit -1
+fi
+
+if [[ "$nb_args" < "1" ]]
+then
+    read -p "Enter a brief desc for this run: " rundesc
+else
+    rundesc="$1"
+fi
+
+
+START_VRUN_SH=$(date '+%s')
---
+++ consolidate_cape_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+prefix="/nfs/fx/home/cwong29/working/NR-scripts"
+nr_prefix="${prefix}/nr-codelets/numerical_recipes"
+saeed_prefix="${prefix}/intel_codelets"
+tmpfile=$(mktemp)
+
+shopt -s extglob
+
+for codelet_dir in ${nr_prefix}/*/*/*_+([[:digit:]])_?e ${saeed_prefix}/*/*/*+([[:digit:]])_?e ; do
+    for cls_dir in $codelet_dir/cls_res_*_*; do
+    if [ -d ${cls_dir} ]; then
+	if [ ! -f ${cls_dir}/counters/*_metrics.cape.csv ]; then
+	    echo "Generate cape format csv at ${cls_dir}"
+	    ./format2cape.sh ${cls_dir} $(hostname)
+	fi
+	# cape file exists
+	capefile="${cls_dir}/counters/*_metrics.cape.csv"
+	head -1 ${capefile} >> ${tmpfile}
+    fi
+    done
+done
+
+echo ${tmpfile}
