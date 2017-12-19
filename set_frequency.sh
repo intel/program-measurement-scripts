262756d334df651c1fc1f3f0c64c64619d3bcc7f
david.c.wong <cwong29@fxilab163.an.intel.com>
2017-12-19 14:27:26 -0600

    Go ahead to check in after disabling Uncore check
    Former-commit-id: 1ba530ca5c33507f35ea49e9f26637d131d82a38 [formerly f766a1ca8310109fd8844a14650565e3e6cb32d6] [formerly 555da1af51346e6372157fdc0d8263e569112fdb [formerly 6d6405529b92165b4384e5004c2e050832e8319c]]
    Former-commit-id: 632efd433d7934abd58d5213cdd866b175300bc4 [formerly b10958bbb804e821b4964ca9a88a65ad001b398f]
    Former-commit-id: 7372a1c487926261cd7f00201cc6e5388e5f5689

--- set_frequency.sh
+++ set_frequency.sh
@@ -111,11 +111,12 @@ then
     ((uncore_bits=hi_uncore_bits<<8|lo_uncore_bits))
     uncore_bits=$(printf "0x%x" ${uncore_bits})
     echo "Setting Uncore Frequency: Min=${min_uncore_frequency}, Max=${max_uncore_frequency} => Uncore bits = ${uncore_bits}"
-    if [[ "$UARCH" == "HASWELL" ]]; then
+# Let's assume we can also set uncore for modern processors
+#    if [[ "$UARCH" == "HASWELL" ]]; then
 	emon --write-msr 0x620="$uncore_bits"
-    else
-        echo "Non-HSW system: uncore freq. setting not done"
-    fi
+#    else
+#        echo "Non-HSW system: uncore freq. setting not done"
+#    fi
 elif [[ -z ${min_uncore_frequency} && -z ${max_uncore_frequency} ]]
 then
     echo "Uncore freq unchanged."
