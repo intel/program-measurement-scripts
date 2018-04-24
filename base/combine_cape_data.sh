2a0d8414496ba4d026f805920565d97003c6a3c9
Wong, David C <david.c.wong@intel.com>
2018-04-24 14:49:17 -0500

    Fixed a type inference bug.
    Former-commit-id: 8bd655755cffc7bf41d48d9ba6551dc14e193f33 [formerly 925df17b076ab50bef4cfd3ac6a762b0b5a1ad50] [formerly 902a721ba8fd252ff2ec283f4c161256f2ed508d [formerly c5f1501ba6cac34841fe4b68c9fa9927853ce7ae]]
    Former-commit-id: 24c51d1722fbfba2a38af680fe4b50224d308dd3 [formerly 604a3a3efe4eb376846f5883cfbdda2b20d84f9e]
    Former-commit-id: 190257dcdd9becc6ee5e22dfb0a7443eb252c568

--- base/combine_cape_data.sh
+++ base/combine_cape_data.sh
@@ -29,9 +29,12 @@ combined_summary_ofile=${run_dir}/cape_summary_${START_VRUN_SH}.csv
 # in2csv --format ndjson combined.jsonl >> combined.csv
 touch $combined_raw_json
 for f in ${ofiles[@]}; do
+    csvjson -I --stream $f
     csvjson -I --stream $f >> $combined_raw_json
 done
-in2csv --format ndjson $combined_raw_json > ${combined_raw_ofile}
+in2csv -I --format ndjson $combined_raw_json
+in2csv -I --format ndjson $combined_raw_json > ${combined_raw_ofile}
+
 
 echo "Running post-processing script to summarize raw data..."
 $CLS_FOLDER/report_summary.py -i ${combined_raw_ofile} -o ${combined_summary_ofile}
