84bebe5e20236f1296313f49c6ae4e6d9c3c87ab
David Wong <david.c.wong@intel.com>
2020-01-28 12:02:22 -0600

    Commented out useless code and printing statements
    Former-commit-id: 7a0d50ae398251be1e7161fc955878c6979f6790 [formerly 91916102e143e04c3558c1a5e0d9080c44eebd2d] [formerly 59d6cb6a86e8fe88fa746337631ecc27ac688306 [formerly 942b7488ddef2d2325e92e83eb0655c64fd997d2]]
    Former-commit-id: 2ddd5308514887fd891c41ccacdb598a19e9c7c2 [formerly 4a72d2ed2b9af1e56ea41fd7cef3f53003e7589d]
    Former-commit-id: 18c1f0c899f04d2df1a11426d94ea064ced1a3f0

--- base/generate_SI.py
+++ base/generate_SI.py
@@ -32,13 +32,13 @@ def plot_data(title, filename, xs, ys):
     # dash_style =
     #     direction, length, (text)rotation, dashrotation, push
     # (The parameters are varied to show their effects, not for visual appeal).
-    dash_style = (
-        (0, 20, -15, 30, 10),
-        (1, 30, 0, 15, 10),
-        (0, 40, 15, 15, 10),
-        (0, 40, 15, 15, 10),
-        (0, 40, 15, 15, 10),
-        (1, 20, 30, 60, 10))
+    #     dash_style = (
+    #         (0, 20, -15, 30, 10),
+    #         (1, 30, 0, 15, 10),
+    #         (0, 40, 15, 15, 10),
+    #         (0, 40, 15, 15, 10),
+    #         (0, 40, 15, 15, 10),
+    #         (1, 20, 30, 60, 10))
     
     fig, ax = plt.subplots()
 
@@ -56,7 +56,7 @@ def plot_data(title, filename, xs, ys):
     #     ctxLabels=list(map(lambda x: "n={}".format(x), ns))
     #     print(ctxLabels)
     text_strs=[str(DATA[i]) for i in range(len(DATA))]
-    print(text_strs)
+    # print(text_strs)
     
     #     for i in range(len(DATA)):
     #         (x, y) = DATA[i]
@@ -86,13 +86,12 @@ ys=[]
 with open (inputfile, 'rU') as input_csvfile:
     csvreader = csv.DictReader(input_csvfile, delimiter=',')
     for input_row in csvreader:
-        print(input_row['x'], input_row['y'])
+        # print(input_row['x'], input_row['y'])
         xs.append(float(input_row['x']))
         ys.append(float(input_row['y']))
 #xs=[1,2,3,4,5,2.5,3,3.5,3.75,3.77]
 #ys=[3,4,1,2,5,2.5,2,2,2,2]
 
-print(xs)
 filename='/tmp/myfig.png' # Set to [] for GUI output
 filename=[]
 plot_data("Sample plot", filename, xs, ys)
