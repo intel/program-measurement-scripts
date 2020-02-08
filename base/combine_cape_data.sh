e45d3fc146310ede368005191a8110e824e22b66
szaday2 <szaday2@deneb.cs.illinois.edu>
2020-02-08 13:06:02 -0600

    moved report_summary to summarize
    Former-commit-id: e38ef1126dbc7205e37aaf23405c9bff30d88bee [formerly 13e91a647724514e339dbe4b676ea800edcf758c] [formerly 86834acf5a6cdde1c06243d41d67421be8fad7cf [formerly cafca84a7b8e415ff7d025e43b7d0b73559fd517]]
    Former-commit-id: 9ec803a8c0136e3b30eeb6f58ce9c3f4dff50f4f [formerly afac0402001204ac901d235dff762e6277275119]
    Former-commit-id: 567370ef3cdde403012311acf82c7990d8b9746c

--- base/combine_cape_data.sh
+++ base/combine_cape_data.sh
@@ -39,7 +39,7 @@ in2csv -I --format ndjson $combined_raw_json > ${combined_raw_ofile}
 
 
 echo "Running post-processing script to summarize raw data..."
-$CLS_FOLDER/report_summary.py -i ${combined_raw_ofile} -o ${combined_summary_ofile}
+$CLS_FOLDER/summarize.py -i ${combined_raw_ofile} -o ${combined_summary_ofile}
 echo "Running Qplot generator to generate QPlot HTML ..."
 $CLS_FOLDER/qplot_data_generation.py -i ${combined_raw_ofile} -o ${combined_qplot_ofile} -q ${combined_qplot_html}
 ${LOGGER_SH} ${START_VRUN_SH} "Cape raw data saved in: ${combined_raw_ofile}"
