e87597cee96626c70f1b4fba5569cfe16f672043
David Wong <david.c.wong@intel.com>
2020-06-08 15:26:25 -0500

    Added Cape Analyzer GUI prototype.
    Former-commit-id: 45eaae1d6fd0e0e241420bd4512a6c1f98de145e [formerly f5205e51d7033aa797be294f65a7b2c1df8af2e1] [formerly b3429c57bbadeaf3ee1c92ab047fa6943810ea1c [formerly c554687b2ef0b6a281c8eed5e2b274b26662c8c2]]
    Former-commit-id: 55e8c7b71b6a5338870ca6ce9f9eabde628d63d1 [formerly 09fee6f9e76c79288f3c87e3f6108dbd0b6ccac1]
    Former-commit-id: 607ec47debcb0cc26b5f8d792880e79cc1376bf3

---
+++ analyzer/analyzer-gui.py
@@ -0,0 +1,234 @@
+import tkinter as tk
+from tkinter import ttk
+from argparse import ArgumentParser
+#from idlelib.TreeWidget import ScrolledCanvas, FileTreeItem, TreeNode
+import pathlib 
+import os
+from os.path import expanduser
+from summarize import summary_report
+from generate_QPlot import parse_ip
+import tempfile
+
+class ScrolledTreePane(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.treeview=ttk.Treeview(self)  
+        vsb = ttk.Scrollbar(self, orient=tk.VERTICAL)
+        vsb.pack(side=tk.RIGHT, fill=tk.Y)
+        hsb = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
+        hsb.pack(side=tk.BOTTOM, fill=tk.X)
+        vsb.configure(command=self.treeview.yview)
+        hsb.configure(command=self.treeview.xview)
+        self.treeview.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)      
+        self.treeview.pack(fill=tk.BOTH, expand=1)  
+
+
+class DataSourcePanel(ScrolledTreePane):
+    class DataTreeNode:
+        nextId = 0
+        nodeDict = {}
+        def __init__(self, name):
+            self.name = name
+            self.id = DataSourcePanel.DataTreeNode.nextId
+            DataSourcePanel.DataTreeNode.nextId += 1
+            DataSourcePanel.DataTreeNode.nodeDict[self.id]=self
+
+        @classmethod
+        def lookupNode(cls, id):
+            return cls.nodeDict[id]
+
+        def open(self):
+            print("node open:", self.name, self.id) 
+
+    class MetaTreeNode(DataTreeNode):
+        def __init__(self, name):
+            super().__init__(name)
+
+    class LocalTreeNode(DataTreeNode):
+        def __init__(self, path, name, container):
+            super().__init__(name)
+            self.container = container
+            self.path = path
+
+    class RemoteTreeNode(DataTreeNode):
+        def __init__(self, name):
+            super().__init__(name)
+
+    class LocalFileNode(LocalTreeNode):
+        def __init__(self, path, name, container):
+            super().__init__(path, name, container)
+
+        def open(self):
+            print("file node open:", self.name, self.id) 
+            self.container.openLocalFile(self.path)
+    
+
+    class LocalDirNode(LocalTreeNode):
+        def __init__(self, path, name, container):
+            super().__init__(path, name+'/', container)
+            self.children = []
+
+        def open(self):
+            print("dir node open:", self.name, self.id) 
+            for d in os.listdir(self.path):
+                if d not in self.children:
+                    self.children.append(d)
+                    fullpath= os.path.join(self.path, d)
+                    if os.path.isdir(fullpath):
+                        self.container.insertNode(self, DataSourcePanel.LocalDirNode(fullpath, d, self.container))
+                    elif os.path.isfile(fullpath):
+                        self.container.insertNode(self, DataSourcePanel.LocalFileNode(fullpath, d, self.container))
+                    else:
+                        print('Skip special file:', d)
+
+
+    def __init__(self, parent, loadDataSrcFn):
+        ScrolledTreePane.__init__(self, parent)
+        self.loadDataSrcFn = loadDataSrcFn
+        self.dataSrcNode = DataSourcePanel.MetaTreeNode('Data Source')
+        self.localNode = DataSourcePanel.MetaTreeNode('Local')
+        self.remoteNode = DataSourcePanel.MetaTreeNode('Remote')
+        self.insertNode(None, self.dataSrcNode)
+        self.insertNode(self.dataSrcNode, self.localNode)
+        self.insertNode(self.dataSrcNode, self.remoteNode)
+
+        self.setupLocalRoots()
+        self.setupRemoteRoots()
+        self.treeview.bind("<<TreeviewOpen>>", self.handleOpenEvent)
+
+    def insertNode(self, parent, node):
+        parent_str = parent.id if parent else ''
+        self.treeview.insert(parent_str,'end',node.id,text=node.name)  
+
+        
+    def openLocalFile(self, source):
+        self.loadDataSrcFn(source)
+
+    def handleOpenEvent(self, event):
+        nodeId = int(self.treeview.focus())
+        node = DataSourcePanel.DataTreeNode.lookupNode(nodeId)
+        node.open()
+
+
+    def setupLocalRoots(self):
+        self.insertNode(self.localNode, DataSourcePanel.LocalDirNode(expanduser("~"), 'Home', self) )
+
+    def openLocalChildren(self):
+        pass
+
+
+    def setupRemoteRoots(self):
+        self.insertNode(self.remoteNode, DataSourcePanel.RemoteTreeNode('uvsq'))
+        self.insertNode(self.remoteNode, DataSourcePanel.RemoteTreeNode('uiuc'))
+        self.insertNode(self.remoteNode, DataSourcePanel.RemoteTreeNode('uta'))
+
+class AnalysisResultsPanel(ScrolledTreePane):
+    def __init__(self, parent):
+        ScrolledTreePane.__init__(self, parent)
+        self.treeview.insert('','0','item1',text='Analysis Results')  
+        self.treeview.insert('','end','item3',text='Local')  
+        self.treeview.insert('item3','end','D',text='Compare QMC against LORE')  
+        self.treeview.insert('item3','end','E',text='Graph algorithm')  
+        self.treeview.insert('item3','end','F',text='OpenCV study')  
+        self.treeview.move('item3','item1','end')    
+
+class ExplorerPanel(tk.PanedWindow):
+    def __init__(self, parent, loadDataSrcFn):
+        tk.PanedWindow.__init__(self, parent, orient="vertical")
+        top = DataSourcePanel(self, loadDataSrcFn)
+        top.pack(side = tk.TOP)
+        self.add(top)
+        bot = AnalysisResultsPanel(self)
+        bot.pack(side = tk.TOP)
+        self.add(bot)
+        self.pack(fill=tk.BOTH,expand=True)
+        self.configure(sashrelief=tk.RAISED)
+
+
+class SummaryTab(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+class ApplicationTab(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+class TrawlTab(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+class QPlotTab(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+class SIPlotTab(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+class AnalyzerGui(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+
+        menubar = tk.Menu(self)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New")#, command=self.configTab.new)
+        filemenu.add_command(label="Open")#, command=self.configTab.open)
+        filemenu.add_command(label="Save")#, command=lambda: self.configTab.save(False))
+        filemenu.add_command(label="Save As...")#, command=lambda: self.configTab.save(True))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit")#, command=self.file_exit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        
+        self.parent.config(menu=menubar)
+
+        pw=tk.PanedWindow(parent, orient="horizontal")
+
+        explorerPanel = ExplorerPanel(pw, self.loadDataSource)
+        explorerPanel.pack(side = tk.LEFT)
+        pw.add(explorerPanel)
+        right = self.buildTabs(pw)
+        right.pack(side = tk.LEFT)
+        pw.add(right)
+        pw.pack(fill=tk.BOTH,expand=True)
+        pw.configure(sashrelief=tk.RAISED)
+
+    def loadDataSource(self, source):
+        print("DATA source to be loaded", source)
+        tmpfile = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
+        print(tmpfile.name)
+        in_files = [source]
+        in_file_format='csv'
+        user_op_file = None
+        request_no_cqa = False
+        request_use_cpi = False
+        request_skip_energy = False
+        request_skip_stalls = False
+        request_succinct = False
+        summary_report(in_files, tmpfile.name, in_file_format, user_op_file, request_no_cqa, \
+            request_use_cpi, request_skip_energy, request_skip_stalls, request_succinct, None)
+        chosen_node_set = set(['L1','L2','L3','RAM','FLOP'])
+        parse_ip(tmpfile.name, "test", "scalar", "Testing", chosen_node_set, False, gui=True)
+
+    def buildTabs(self, parent):
+        note = ttk.Notebook(parent)
+        summaryTab = SummaryTab(note)
+        appTab = ApplicationTab(note)
+        trawlTab = TrawlTab(note)
+        qplotTab = QPlotTab(note)
+        siPlotTab = SIPlotTab(note)
+        note.add(summaryTab, text="Summary")
+        note.add(appTab, text="Application")
+        note.add(trawlTab, text="TRAWL")
+        note.add(qplotTab, text="QPlot")
+        note.add(siPlotTab, text="SI Plot")
+        note.pack()
+        return note
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Cape Analyzer')
+    root = tk.Tk()
+    gui = AnalyzerGui(root)
+
+    root.mainloop()
