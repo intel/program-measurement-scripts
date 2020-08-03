271fd15888ec5dd2d3468dc2eb553a96990ae448
David Wong <david.c.wong@intel.com>
2020-08-03 14:28:26 -0500

    Updated math.mod() to math.fmod() - due to updated MAQAO using new LUA version.
    Former-commit-id: 6e2f24e06829e95f96a1d709dda491b0b0f372cc [formerly dbc560bdfeadfdefd65fe21b0674c8f7c8a9e129] [formerly a325f7e01a073c03d446c6f975f8c075c9656d74 [formerly d1cfb1812fe1bbd37b42d067b10c292a09964e83]]
    Former-commit-id: 346f36856f44993105502b583e37eebf9afdb89d [formerly 7cd65e5a4dfaf2cc4aabefb7763b57d28d7a35ac]
    Former-commit-id: 8b1bf99e775c0a0d83eb8cd64714a04e0108ead4

--- utils/MAQAO/maqao_new_2020/generator.lua
+++ utils/MAQAO/maqao_new_2020/generator.lua
@@ -10,7 +10,7 @@ function DEC_HEX (IN)
     local B, K, OUT, I, D=16, "0123456789abcdef", "", 0
     while IN > 0 do
         I = I + 1
-        IN, D = math.floor (IN / B), math.mod (IN, B) + 1
+        IN, D = math.floor (IN / B), math.fmod (IN, B) + 1
         OUT = string.sub (K,D,D) .. OUT
     end
     return OUT
