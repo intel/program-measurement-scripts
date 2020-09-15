ae5559a81a00e8f15528924d12e1b1cf26a8394a
Wong <david.c.wong@intel.com>
2020-09-14 19:32:56 -0500

    Updated to add GUI

--- analyzer/guide_fsm.py
+++ analyzer/guide_fsm.py
@@ -1,4 +1,6 @@
+import tkinter as tk
 import enum
+
 #from transitions import Machine
 from transitions.extensions import GraphMachine as Machine
 from abc import ABC, abstractmethod
@@ -95,13 +97,73 @@ transitions = [['proceed', States.INIT, States.Astart],
 # Uses transitions package to implement the finite machine taking care of state transition
 # and then use State design pattern to handle different behaviour of different states
 
+# Simple implementation of Observer Design Pattern
+class Observable:
+    def __init__(self):
+        self.observers = []
+    def add_observers(self, observer):
+        self.observers.append(observer)
+    def notify_observers(self):
+        for observer in self.observers:
+            observer.notify(self)
+            
+class Observer:
+    def __init__(self, observable):
+        observable.add_observers(self)
+        
+    def notify(self, observable):
+        print("Notified from ", observable)
+
+class FiniteStateMachine(Observable):
+    def __init__(self):
+        super().__init__()
+        m = Machine(self, states=States, transitions=transitions, initial=States.INIT, use_pygraphviz=False, after_state_change=self.notify_observers)
+        self.fsm = m
+        self.file = 'c:/users/cwong29/AppData/Local/Temp/my_state_diagram1.png'
+        self.dump_graph()
+ #       assert self.is_INIT()
+ #       assert self.state is States.INIT
+ #       state = m.get_state(States.INIT)  # get transitions.State object
+ #       print(state.name)  # >>> INIT
+ #       self.proceed()
+ #       self.proceed()
+ #       assert self.is_Aend()
+
+    def dump_graph(self):
+        self.fsm.get_graph().draw(self.file, prog='dot')
+
+
+class Gui(tk.Frame):
+    def __init__(self, parent, fsm):
+        tk.Frame.__init__(self, parent, width=1500, height=500)
+        self.pack()
+        self.fsm = fsm
+        self.fsm.add_observers(self)
+        self.canvas = tk.Canvas(self, width=1500, height=500)
+        self.canvas.pack(side=tk.BOTTOM)
+        self.img = tk.PhotoImage(file=fsm.file)
+        self.canvas.create_image(750, 250, image=self.img)
+
+        bottomframe = tk.Frame(self)
+        bottomframe.pack(side=tk.BOTTOM)
+
+        proceedButton = tk.Button(bottomframe, text="Proceed", command=self.fsm.proceed)
+        proceedButton.pack(side=tk.LEFT)
+        detailsButton = tk.Button(bottomframe, text="Details", command=self.fsm.details)
+        detailsButton.pack(side=tk.LEFT)
+        
+    def notify(self, observable):
+        self.fsm.dump_graph()
+        print("changed")
+        self.img = tk.PhotoImage(file=self.fsm.file)
+        self.canvas.create_image(750, 250, image=self.img)
+
+
+
 
 if __name__ == '__main__':
-    m = Machine(states=States, transitions=transitions, initial=States.INIT, use_pygraphviz=False)
-    assert m.is_INIT()
-    assert m.state is States.INIT
-    state = m.get_state(States.INIT)  # get transitions.State object
-    print(state.name)  # >>> INIT
-    m.proceed()
-    m.proceed()
-    assert m.is_Aend()
+    fsm = FiniteStateMachine()
+
+    root = tk.Tk()
+    gui = Gui(root, fsm)
+    root.mainloop()
