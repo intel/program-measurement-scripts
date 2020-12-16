06bbcc6b0b84506d4340c732b109738ee7c0be15
David Wong <david.c.wong@intel.com>
2020-12-16 17:33:58 -0600

    Added test for matrix norm

--- tests/test_base.py
+++ tests/test_base.py
@@ -19,7 +19,18 @@ class TestSiMethods(unittest.TestCase):
         self.assertIn('Saturation', new_all_df.columns)
         self.assertIn('Intensity', new_all_df.columns)
         self.assertTrue(new_cluster_df.append(new_run_df).equals(new_all_df))
-        #self.asser 
+        # Should add more to check values
+
+    def test_compute_norm_matrix(self):
+        root = os.path.join(os.path.dirname(__file__), 'data', 'generate_SI')
+        cur_run_df = pd.read_csv(os.path.join(root, 'Test.csv'))
+        cur_run_df[NonMetricName.SI_SAT_NODES]=[['L1 [GB/s]', 'L2 [GB/s]', 'L3 [GB/s]', 'FLOP [GFlop/s]']]*len(cur_run_df)
+        cluster_df = pd.read_csv(os.path.join(root, 'LORE-Optimal.csv'))
+        new_cluster_df, new_all_df, new_run_df = compute_only(cluster_df, 'matrix', cur_run_df)
+        self.assertIn('Saturation', new_all_df.columns)
+        self.assertIn('Intensity', new_all_df.columns)
+        self.assertTrue(new_cluster_df.append(new_run_df).equals(new_all_df))
+        # Should add more to check values
 
 if __name__ == '__main__':
     unittest.main()
