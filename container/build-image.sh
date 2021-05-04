436fe3bcd1c51c21cdd103f73266be5d205b2458
David Wong <david.c.wong@intel.com>
2021-05-04 14:52:31 -0500

    Following Justin's website desc to build tools - see: https://gitlab.com/Wingpad/rose-utils

--- container/build-image.sh
+++ container/build-image.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+mkdir -p container/rose-utils
+pushd container/rose-utils
+# User the image builder's .ssh
+#git clone git@gitlab.com:Wingpad/rose-utils.git
+popd
+
+#docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t capeexperimentscripts:latest "container"
 docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t capeexperimentscripts:latest "container"
