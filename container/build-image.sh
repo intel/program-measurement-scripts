ce9a95438b77cf5241c11705eb3fa607a110c02b
David Wong <david.c.wong@intel.com>
2021-05-21 14:24:04 -0500

    Added steps to build and push image to gitlab.com

--- container/build-image.sh
+++ container/build-image.sh
@@ -10,3 +10,10 @@ popd
 #docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t capeexperimentscripts:latest "container"
 # Below assums proxy servers are needed to access the network
 docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t capeexperimentscripts:latest "container"
+
+# Also build the image to Gitlab
+# login only need to be done once for password entering
+docker login registry.gitlab.com
+
+docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t registry.gitlab.com/davidwong/cape-experiment-scripts  "container"
+docker push registry.gitlab.com/davidwong/cape-experiment-scripts
