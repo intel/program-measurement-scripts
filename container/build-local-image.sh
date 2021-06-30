4278a6fec89ac29d35f81373768a94f7c27f3846
David Wong <david.c.wong@intel.com>
2021-06-30 18:39:13 -0500

    Added proxy settings

--- container/build-local-image.sh
+++ container/build-local-image.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build --build-arg LOCAL_UID=$(id -u ${USER}) --build-arg LOCAL_GID=$(id -g ${USER}) --pull --rm -f ./LocalDockerfile -t local_image .
+# Fetch latest image
+docker pull registry.gitlab.com/davidwong/cape-experiment-scripts:latest
+
+docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg LOCAL_UID=$(id -u ${USER}) --build-arg LOCAL_GID=$(id -g ${USER}) --pull --rm -f ./LocalDockerfile -t local_image .
