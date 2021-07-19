feb5d7860ed3e47b553794333f79974828866063
David Wong <david.c.wong@intel.com>
2021-07-19 17:53:28 -0500

    Updated to ensure proxy variables start with http://

--- container/build-local-image.sh
+++ container/build-local-image.sh
@@ -7,4 +7,16 @@ docker login registry.gitlab.com
 # Fetch latest image
 docker pull registry.gitlab.com/davidwong/cape-experiment-scripts:latest
 
-docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg LOCAL_UID=$(id -u ${USER}) --build-arg LOCAL_GID=$(id -g ${USER}) --pull --rm -f ./LocalDockerfile -t local_image .
+if [[ $http_proxy != http* ]]; then
+  http_proxy_arg=http://${http_proxy}
+else
+  http_proxy_arg=${http_proxy}
+fi
+
+if [[ $https_proxy != http* ]]; then
+  https_proxy_arg=http://${https_proxy}
+else
+  https_proxy_arg=${https_proxy}
+fi
+
+docker build --build-arg http_proxy=$http_proxy_arg --build-arg https_proxy=$https_proxy_arg --build-arg LOCAL_UID=$(id -u ${USER}) --build-arg LOCAL_GID=$(id -g ${USER}) --pull --rm -f ./LocalDockerfile -t local_image .
