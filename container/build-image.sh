f3e0b9f1ff30014ec81787f5d9e76e1dcdbfb2e4
David Wong <david.c.wong@intel.com>
2021-07-19 17:27:28 -0500

    Added parameter parsing - also ability to choose not to push image.

--- container/build-image.sh
+++ container/build-image.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-#rm -rf container/rose-utils
-#mkdir -p container/rose-utils
-#pushd container/rose-utils
+push_image=false
+while getopts ":p" opt; do
+  case ${opt} in
+  p)
+    push_image=true
+    ;;
+  \?)
+    echo "Usage: $0 [-p]"
+    exit
+    ;;
+  esac
+done
+
+rm -rf rose-utils
+#mkdir -p rose-utils
+#pushd rose-utils
 # User the image builder's .ssh
-#git clone git@gitlab.com:Wingpad/rose-utils.git
+git clone https://gitlab.com/Wingpad/rose-utils.git --config core.autocrlf=input
 #popd
+
+git clone https://bitbucket.org/thiagotei/ice-locus-dev.git  --config core.autocrlf=input
 
 #docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t capeexperimentscripts:latest "container"
 # Below assums proxy servers are needed to access the network
-docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f ".\Dockerfile" -t capeexperimentscripts:latest "."
+#docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f ".\Dockerfile" -t capeexperimentscripts:latest "."
 
-# Also build the image to Gitlab
 # login only need to be done once for password entering
 docker login registry.gitlab.com
-
+# Also build the image to Gitlab
 docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f ".\Dockerfile" -t registry.gitlab.com/davidwong/cape-experiment-scripts  "."
-docker push registry.gitlab.com/davidwong/cape-experiment-scripts
+
+if [[ $push_image = true ]]; then
+  docker push registry.gitlab.com/davidwong/cape-experiment-scripts
+fi
