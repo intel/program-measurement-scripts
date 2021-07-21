e02bd3320e2b7c645681474bdf615da878efdf10
David Wong <david.c.wong@intel.com>
2021-07-21 15:53:11 -0500

    Updated local image building to include host user groups

--- container/build-local-image.sh
+++ container/build-local-image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Ensure user logged in
+local_gids=$(id -G)
+local_gnames=$(id -Gn)
 
 echo "Logging into registry.gitlab.com... (it may ask for gitlab.com password if not done before)"
 docker login registry.gitlab.com
@@ -19,4 +21,4 @@ else
   https_proxy_arg=${https_proxy}
 fi
 
-docker build --build-arg http_proxy=$http_proxy_arg --build-arg https_proxy=$https_proxy_arg --build-arg LOCAL_UID=$(id -u ${USER}) --build-arg LOCAL_GID=$(id -g ${USER}) --pull --rm -f ./LocalDockerfile -t local_image .
+docker build --build-arg http_proxy=$http_proxy_arg --build-arg https_proxy=$https_proxy_arg --build-arg LOCAL_UID=$(id -u ${USER}) --build-arg LOCAL_GID=$(id -g ${USER}) --build-arg LOCAL_GIDS="$local_gids" --build-arg LOCAL_GNAMES="$local_gnames" --pull --rm -f ./LocalDockerfile -t local_image .
