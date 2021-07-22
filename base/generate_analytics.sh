6f2ee66791c3a0405b36a41be97ec879bd82775a
David Wong <david.c.wong@intel.com>
2021-07-22 15:28:21 -0500

    use ctags to find source file given fn name

--- base/generate_analytics.sh
+++ base/generate_analytics.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 nb_args="$#"
-if [[ "$nb_args" != "2" ]]; then
-	echo "ERROR! Got $nb_args arguments, expected: source file and output path"
+if [[ "$nb_args" != "3" ]]; then
+	echo "ERROR! Got $nb_args arguments, expected: source file, function name and output path"
+	echo "Got: $*"
 	exit -1
 fi
 
 # get the absolute path for the arguments
+# code_path contains path to folder containing source file
 code_path="$(realpath $1)"
-output_path="$(realpath $2)"
+function_name="$2"
+output_path="$(realpath $3)"
+
+# The output format of ctags is
+# <symbol> <type> <line number> <filename> <source>
+# So below check for column 1 for function name and then return column 4 which is filename
+code_file=$(ctags -x ${code_path}/*  |grep -E "function|subroutine" |awk '$1 == "'${function_name}'" {print $4}')
+
 
 # fail if the extension does not match expectation
 if [[ "$output_path" != *csv ]]; then
@@ -20,7 +29,7 @@ fi
 rm -f $output_path
 
 # validate we're operating with a supported file extension
-code_name="$(basename $code_path | tr '[:upper:]' '[:lower:]')" 
+code_name="$(basename $code_file | tr '[:upper:]' '[:lower:]')" 
 if [[ "$code_name" != *c ]] && [[ "$code_name" != *cc ]] && [[ "$code_name" != *cpp ]]; then
 	exit 0
 fi
@@ -37,5 +46,5 @@ if [[ -f "$unitool_path/batch_run.py" ]]; then
     # unitool may be limited to running under its own path
     cd $unitool_path
     # it outputs a csv to stdout, that we redirect to the output path
-    ./batch_run.py $code_path > $output_path
+    ./batch_run.py $code_file > $output_path
 fi
