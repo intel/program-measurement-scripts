10a7e593ee56209f483eebf9b6832b1d91cf82dd
David Wong <david.c.wong@intel.com>
2021-07-28 15:53:53 -0500

    Update to skip folder if exists.

--- container/build-image.sh
+++ container/build-image.sh
@@ -13,14 +13,25 @@ while getopts ":p" opt; do
   esac
 done
 
-rm -rf rose-utils
+#rm -rf rose-utils
 #mkdir -p rose-utils
 #pushd rose-utils
 # User the image builder's .ssh
-git clone https://gitlab.com/Wingpad/rose-utils.git --config core.autocrlf=input
+if [ ! -d rose-utils ]; then
+  git clone https://gitlab.com/Wingpad/rose-utils.git --config core.autocrlf=input
 #popd
+fi
 
-git clone https://bitbucket.org/thiagotei/ice-locus-dev.git  --config core.autocrlf=input
+if [ ! -d ice-locus-dev ]; then
+  git clone https://bitbucket.org/thiagotei/ice-locus-dev.git  --config core.autocrlf=input
+fi
+
+if [ ! -d pocc-1.1 ]; then
+  curl -O http://web.cs.ucla.edu/~pouchet/software/pocc/download/pocc-1.1-full.tar.gz
+  tar xvf pocc-1.1-full.tar.gz
+  cd pocc-1.1
+  curl -O https://bitbucket.org/thiagotei/uiuc-compiler-opts/raw/39556c88b86e6a7e727117183c93906ab89ffeb1/pocc-1.1-candl-0.6.2.patch
+fi
 
 #docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --rm -f "container\Dockerfile" -t capeexperimentscripts:latest "container"
 # Below assums proxy servers are needed to access the network
