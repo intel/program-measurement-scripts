96e7c94486de8fa581bc735d1074c1f090f84f54
David Wong <david.c.wong@intel.com>
2021-08-03 18:31:35 -0500

    Added implementations for new EMON API calls.

--- utils/codeletProbe/emon_api/emon_api_hook.c
+++ utils/codeletProbe/emon_api/emon_api_hook.c
@@ -3,44 +3,63 @@
 
 ///* Intel's SEP header to perform per region measurement*/
 //#include <sampling.h>
-#ifdef HAS_EMON_API
+#if defined(HAS_OLD_EMON_API) || defined(HAS_NEW_EMON_API)
 #include <emon_api.h>
 #endif
 
-//#ifdef HAS_EMON_API
+//#ifdef HAS_OLD_EMON_API
 //static char *emon_api_stream = "emon_api.out";
 //#endif
 
-#ifdef HAS_EMON_API
+#ifdef HAS_OLD_EMON_API
 static EMON_HANDLE emon_handle;
+#endif
+
+#if defined(HAS_OLD_EMON_API) || defined(HAS_NEW_EMON_API)
 static EMON_DATA   emon_data_before, emon_data_after, emon_data_result, emon_data_sum;
 #endif
 
 void init_hook() {
-#ifdef HAS_EMON_API
+#ifdef HAS_OLD_EMON_API
            EMONConfig ("emon_api_config_file", &emon_handle);
 
            EMONStart (&emon_handle);
 	   emon_data_sum = EMON_DATA_NULL;
+#elif defined(HAS_NEW_EMON_API)
+           EMONConfig ("emon_api_config_file");
+
+           EMONStart ();
+	   // subtracting emon_data_before by emon_data_after with 0 as multiplier to set it to zero.
+	   // cannot just do sum *0 -sum*0 because of EMONDataCalculate seems to deallocate argument twice
+	   EMONReadCounts (&emon_data_before);
+	   EMONReadCounts (&emon_data_after);
+	   EMONDataCalculate (emon_data_before, emon_data_after, 0, 0, EMON_API_OPERATION_SUBTRACTION, &emon_data_sum);
+
 #endif
 }
 
 void measure_start_hook() {
-#ifdef HAS_EMON_API
+#ifdef HAS_OLD_EMON_API
 		EMONRead (emon_handle, &emon_data_before);
+#elif defined(HAS_NEW_EMON_API)
+		EMONReadCounts (&emon_data_before);
 #endif
 }
 
 void measure_pause_hook() {
-#ifdef HAS_EMON_API
+#ifdef HAS_OLD_EMON_API
 		EMONRead (emon_handle, &emon_data_after);
 		EMONDataCalculate (emon_handle, emon_data_before, emon_data_after, &emon_data_result, "SUBTRACTION");
 		EMONDataCalculate (emon_handle, emon_data_result, emon_data_sum, &emon_data_sum, "ADDITION");
+#elif defined(HAS_NEW_EMON_API)
+		EMONReadCounts (&emon_data_after);
+		EMONDataCalculate (emon_data_after, emon_data_before, 1, 1, EMON_API_OPERATION_SUBTRACTION, &emon_data_result);
+		EMONDataCalculate (emon_data_sum, emon_data_result, 1, 1, EMON_API_OPERATION_ADDITION, &emon_data_sum);
 #endif
 }
 
 void measure_print_data_hook() {
-#ifdef HAS_EMON_API
+#ifdef HAS_OLD_EMON_API
 	// open file to redirect to
 //	int emon_api_fd = open (emon_api_stream, O_WRONLY | O_CREAT| O_APPEND, S_IRUSR|S_IWUSR);
 //	if (emon_api_fd < 0) {
@@ -66,13 +85,17 @@ void measure_print_data_hook() {
 //		return;
 //	}
 //	close(emon_api_fd);
+#elif defined(HAS_NEW_EMON_API)
+	EMONDataPrintAll (emon_data_sum);
 #endif
 
 }
 
 void measure_stop_hook() {
-#ifdef HAS_EMON_API
+#ifdef HAS_OLD_EMON_API
 	EMONStop(&emon_handle);
+#elif defined(HAS_NEW_EMON_API)
+	EMONStop();
 #endif
 
 }
